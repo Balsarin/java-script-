f28237b6f957be00c5b743710f167c3c4086d285
Balsarin <balsarin.pinto@escola.pr.gov.br>
2026-05-08 09:29:43 -0300

    update

---
+++ semana2.js
@@ -0,0 +1,23 @@
+// Aula semana 2 
+
+
+const adicionar = (lista) => {
+    lista.push(fruta)
+    return lista 
+}
+let frutas = ["Uva","Limão"]
+frutas = adicionar(frutas,"abacaxi")
+console.log(fruta)
+
+const remover = (lista) => {
+    lista.push()
+    return lista 
+}
+
+const ordemUltimoParaPrimeiro = (lista) => {
+    let temp = lista[0]
+    lista[0] = lista [-1]
+    lista [-1] = temp
+    return lista
+
+}
